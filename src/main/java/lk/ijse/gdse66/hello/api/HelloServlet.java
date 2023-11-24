3008be169189f751b6c035665c27921e1f27d708
LahiruHarshana <lharshana2002@gmail.com>
2023-11-24 13:49:17 +0530

    updated

--- src/main/java/lk/ijse/gdse66/hello/api/HelloServlet.java
+++ src/main/java/lk/ijse/gdse66/hello/api/HelloServlet.java
@@ -10,6 +10,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Incoming request"+req.getRemoteAddr());
-        resp.getWriter().println("Hello world!");
+        resp.getWriter().println("Hello I am from the server");
     }
 }
